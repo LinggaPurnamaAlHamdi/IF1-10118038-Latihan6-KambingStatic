e32bc95b21c33474c3243270d2556f7869e9ba17
LinggaPurnamaAlHamdi <55777688+LinggaPurnamaAlHamdi@users.noreply.github.com>
2019-09-25 12:35:43 +0700

    Add files via upload

---
+++ IF1-10118038-Latihan6-KambingStatic/src/if1/pkg10118038/latihan6/kambingstatic/IF110118038Latihan6KambingStatic.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118038.latihan6.kambingstatic;
+
+/**
+ *
+ * @author Lingga
+ * NAMA : Lingga Purnama Al Hamdi
+ * KELAS : IF1
+ * NIM : 10118038
+ * Deskripsi Program : program ini berisi program yang menampilkan jumlah 
+ * kambing static
+
+ */
+
+
+public class IF110118038Latihan6KambingStatic {
+    
+  //NAMA_KAMBING sebagai konstanta
+    public static final String NAMA_KAMBING = "MIDUN";
+    
+    public static void main(String[] args) {
+        Mamalia.jumlahKambing = 485000;
+        System.out.println(NAMA_KAMBING + " memiliki kambing sebanyak " + Mamalia.jumlahKambing);
+    }
+    
+}
---
+++ IF1-10118038-Latihan6-KambingStatic/src/if1/pkg10118038/latihan6/kambingstatic/Mamalia.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118038.latihan6.kambingstatic;
+
+/**
+ *
+ * @author Lingga
+ */
+public class Mamalia {
+    //Variabel jumlah kambing dideklarasikan sebagai statik 
+    public static int jumlahKambing;
+}
